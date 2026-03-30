ddec92a40991e634a33510f12cc9b76193a638ca
kentliewkentlim-beep <kent.liewkentlim@gmail.com>
2026-03-30 17:51:53 +0800

    Unify nav to top bar (remove sidebar)

--- src/app/supervisor/layout.tsx
+++ src/app/supervisor/layout.tsx
@@ -1,6 +1,5 @@
 import { getCurrentUser } from "@/lib/auth";
 import { redirect } from "next/navigation";
-import Sidebar from "@/components/Sidebar";
 import Navbar from "@/components/Navbar";
 
 export default async function SupervisorLayout({
@@ -14,16 +13,13 @@ export default async function SupervisorLayout({
   }
 
   return (
-    <div className="flex h-screen bg-slate-50 dark:bg-slate-950 overflow-hidden">
-      <Sidebar role={user.role} />
-      <div className="flex-1 flex flex-col overflow-hidden">
-        <Navbar user={user} />
-        <div className="flex-1 overflow-y-auto p-4 sm:p-6 lg:p-8">
-          <div className="max-w-7xl mx-auto">
-            {children}
-          </div>
+    <div className="min-h-[100dvh] bg-slate-50 dark:bg-slate-950 flex flex-col">
+      <Navbar user={user} />
+      <main className="flex-1 p-4 sm:p-6 lg:p-8">
+        <div className="max-w-7xl mx-auto">
+          {children}
         </div>
-      </div>
+      </main>
     </div>
   );
 }
